8e75aebdd05d473f98a7c7d44948cb516a7dbf8a
Franco Zeta <perufra@hotmail.com>
2024-09-19 09:12:22 -0500

    Add lib/ui components to integrate shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import LoginPage from './pages/LoginPage'
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1>Music Box</h1>
+      <h1>Music Box</h1>
+      <LoginPage />
     </>
   )
 }
---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { FaGoogle, FaFacebookF, FaGithub } from 'react-icons/fa'
+
+const LoginPage = () => {
+  return (
+    //TODO: Login Page should be created by shadecn's components
+    <h1>LoginPage Here!</h1>
+  )
+}
+
+export default LoginPage
