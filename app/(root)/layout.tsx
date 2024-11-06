26abb27977c5622abd8033fd3722b8b72bbffa70
Franco Zeta <perufra@hotmail.com>
2024-11-06 17:23:54 -0500

    Implement authentication layout and pages; add ClerkProvider and sign-in/sign-up components

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,56 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "../globals.css";
+
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import Topbar from '@/components/shared/Topbar';
+import LeftSidebar from '@/components/shared/LeftSidebar';
+import RightSidebar from '@/components/shared/RightSidebar';
+import Botttombar from '@/components/shared/Botttombar';
+
+const geistSans = localFont({
+  src: "../fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "../fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "MusicBox",
+  description: "App dedicated to music reviewers",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark
+      }}
+    >
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Topbar />
+          <main className='flex flex-row'>
+            <LeftSidebar />
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+          <Botttombar />
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className='head-text text-left'>Home</h1>
+
+      <section className='mt-9 flex flex-col gap-10'>
+        
+      </section>
+
+    </>
+
+  );
+}
