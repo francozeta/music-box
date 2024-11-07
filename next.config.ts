4cf599f1fe2ef4eae009fb9847fbacf7eee1549e
Franco Zeta <perufra@hotmail.com>
2024-11-06 19:23:31 -0500

    Add file upload functionality with middleware and configuration updates

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,39 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["moongose"]
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.clerk.dev",
+      },
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "media.istockphoto.com",
+      }
+    ]
+  }
 };
 
 export default nextConfig;
