1940a2caa59b6a808fc49854402d34b803e13334
Franco Zeta <perufra@hotmail.com>
2024-11-25 12:11:00 -0500

    Remove uploadthing.ts file and its associated upload helpers

--- lib/uploadthing.ts
+++
@@ -1,7 +0,0 @@
-// Resource: https://v6.docs.uploadthing.com/api-reference/react#generatereacthelpers
-
-import { generateReactHelpers } from "@uploadthing/react";
-
-import type { OurFileRouter } from '@/app/api/upload/core'; 
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<OurFileRouter>();
