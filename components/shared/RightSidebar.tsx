f03540c8878390d65934fde70aca13cd83fe8c83
Franco Zeta <perufra@hotmail.com>
2024-12-03 21:26:17 -0500

    Fetch and display similar reviewers in RightSidebar; update layout and handling for user suggestions

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,16 +1,40 @@
+import { fetchUsers } from '@/lib/actions/user.actions'
+import { currentUser } from '@clerk/nextjs/server'
+import UserCard from '../cards/UserCard'
 
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const user = await currentUser()
+  if (!user) return null
+
+
+  const similarMinds = await fetchUsers({
+    userId: user.id,
+    pageSize: 6,
+  })
   return (
     <section className='custom-scrollbar rightsidebar'>
       <div className="flex flex-1 flex-col justify-start">
         <h3 className='text-heading-4-medium text-light-1'>
-          Suggested Communities
+          Similar Reviewers
         </h3>
-      </div>
-      <div className="flex flex-1 flex-col justify-start">
-        <h3 className='text-heading-4-medium text-light-1'>
-          Suggested Reviewers
-        </h3>
+        <div className="mt-7 flex w-[350px] flex-col gap-10">
+          {similarMinds.users.length > 0 ? (
+            <>
+              {similarMinds.users.map((person) => (
+                <UserCard
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  username={person.username}
+                  imgUrl={person.image}
+                  personType='User'
+                />
+              ))}
+            </>
+          ) : (
+            <p className='!text-base-regular text-light-3'>No users yet</p>
+          )}
+        </div>
       </div>
     </section>)
 }
